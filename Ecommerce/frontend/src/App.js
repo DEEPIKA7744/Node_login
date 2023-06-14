ad12c8a0f682e06d3249b8fb9620f871e6aea90d
DEEPIKA7744 <deepikaraja11072001@gmail.com>
2023-06-14 14:42:27 +0530

    added

---
+++ Ecommerce/frontend/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Shopify</a>
+        
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
